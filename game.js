46037664d45af317b6c77be11ba48d31a30f590c
Itsirk <kld1532@live.com>
2020-07-07 11:39:43 -0400

    Answers
    Color the correct and incorrect answers

--- game.js
+++ game.js
@@ -101,7 +101,19 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
+        
+        const classToApply = 'incorrect';
+        if (selectedAnswer == currentQuestion.answer) {
+            classToApply = 'correct';
+        }
+
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout( () => {  
+        selectedChoice.parentElement.classList.remove(classToApply);
         getNewQuestion();
+    }, 1000);
     });
 });
 
