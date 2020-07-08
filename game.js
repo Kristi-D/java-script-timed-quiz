455ffea83d7ead42751d3a102421b0efe5391064
Itsirk <kld1532@live.com>
2020-07-08 12:06:45 -0400

    Questions and Answers
    Added questions and answers

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+var question = document.getElementById("question");
+var choices = Array.from(document.getElementsByClassName('choice-text'));
+var progressText = document.getElementById('progressText');
+var scoreText = document.getElementById('score');
+
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -10,51 +13,51 @@ let availableQuestions = [];
 
 let questions = [
     {
-        question: "Question 1?",
-        choice1: "Answer A for Question 1",
-        choice2: "Answer B for Question 1",
-        choice3: "Answer C for Question 1",
-        choice4: "Answer D for Question 1",
-        answer: 1
-    },
-
-
-    {
-        question: "Question 2?",
-        choice1: "Answer A for Question 2",
-        choice2: "Answer B for Question 2",
-        choice3: "Answer C for Question 2",
-        choice4: "Answer D for Question 2",
-        answer: 3
-    },
-
-
-    {
-        question: "Question 3?",
-        choice1: "Answer A for Question 3",
-        choice2: "Answer B for Question 3",
-        choice3: "Answer C for Question 3",
-        choice4: "Answer D for Question 3",
-        answer: 4
-    },
-
-
-    {
-        question: "Question 4?",
-        choice1: "Answer A for Question 4",
-        choice2: "Answer B for Question 4",
-        choice3: "Answer C for Question 4",
-        choice4: "Answer D for Question 4",
+        question: "JavaScript written under which of the following tag?",
+        choice1: "<JavaScript></JavaScript>",
+        choice2: "<script></script>",
+        choice3: "<code></code>",
+        choice4: "<head></head>",
         answer: 2
     },
 
 
     {
-        question: "Question 5?",
-        choice1: "Answer A for Question 5",
-        choice2: "Answer B for Question 5",
-        choice3: "Answer C for Question 5",
-        choice4: "Answer D for Question 5",
+        question: "Variable in JavaScript declared with which of the following keyword?",
+        choice1: "new",
+        choice2: "int",
+        choice3: "var",
+        choice4: "string",
+        answer: 3
+    },
+
+
+    {
+        question: "Which of the followings are primitive data types in JavaScript?",
+        choice1: "String",
+        choice2: "Number",
+        choice3: "Boolean",
+        choice4: "All of the above",
+        answer: 4
+    },
+
+
+    {
+        question: "What is null in JavaScript",
+        choice1: "Null means empty string value",
+        choice2: "Null means unknown value",
+        choice3: "Null means absence of a value",
+        choice4: "Null means zero value",
+        answer: 3
+    },
+
+
+    {
+        question: "Which of the following is not a valid keyword in JavaScript?",
+        choice1: "module",
+        choice2: "function",
+        choice3: "this",
+        choice4: "try",
         answer: 1
     },
 
@@ -62,8 +65,8 @@ let questions = [
 
 
 //constants
-const CORRECT_Bonus = 10;
-const MAX_QUESTIONS = 3;
+var CORRECT_Bonus = 10;
+var MAX_QUESTIONS = 5;
 
 startGame = () => {
     questionCounter = 0;
@@ -99,24 +102,33 @@ choices.forEach(choice => {
         if(!acceptingAnswers) return;
 
         acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        var selectedChoice = e.target;
+        var selectedAnswer = selectedChoice.dataset["number"];
         
-        const classToApply = 'incorrect';
+        var classToApply = 'incorrect';
         if (selectedAnswer == currentQuestion.answer) {
             classToApply = 'correct';
         }
 
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_Bonus);
 
+        }
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout( () => {  
         selectedChoice.parentElement.classList.remove(classToApply);
         getNewQuestion();
-    }, 1000);
+        }, 1000);
     });
 });
 
+
+incrementScore = num => {
+
+    score += num;
+    scoreText.innerText = score;
+}
 startGame();
 
 
